e9ffb38ec0e82b354671daea8b850cc98c1d5bb3
dayhim58 <dayhim58@gmail.com>
2024-12-06 23:30:34 +0330

    Adding "the first object and script"

---
+++ RacingCar/Assets/Driver.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 0.01f;
+    void Start()
+    {
+        if (moveSpeed <= 0)
+        {
+            Debug.LogError("Move Speed cannot be zero or negative");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
+    }
+
+}
